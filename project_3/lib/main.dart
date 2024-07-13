8920b087a9d5f5efce93afb3bc339d4bb25307eb
Pratham <jadhavprathmesh284@gmail.com>
2024-07-13 20:31:14 +0530

    .

--- project_3/lib/main.dart
+++ project_3/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:project_3/assignment2.dart';
+import 'package:project_3/assignment3.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Assignment2(),
+      home: Assignment3(),
     );
   }
 }
