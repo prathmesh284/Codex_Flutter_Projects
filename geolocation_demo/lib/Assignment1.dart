4e5b2a7e1248ed75538130116bc73fa941d8662d
Pratham <jadhavprathmesh284@gmail.com>
2024-08-20 20:40:21 +0530

    development of geolocation demo app

---
+++ geolocation_demo/lib/Assignment1.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Assignment1 extends StatefulWidget {
+  const Assignment1({super.key});
+
+  @override
+  State<Assignment1> createState() => _Assignment1State();
+}
+
+class _Assignment1State extends State<Assignment1> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
